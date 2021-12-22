26f6e82301646041ba5f07211ad9f2506265fb5e
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-22 08:47:36 -0500

    explicit imports in init files

--- pytealutils/math/__init__.py
+++ pytealutils/math/__init__.py
@@ -1,1 +1,1 @@
-from .math import *
+from .math import exp10, max, min, div_ceil
--- pytealutils/storage/__init__.py
+++ pytealutils/storage/__init__.py
@@ -1,2 +1,2 @@
 from .blob import *
-from .storage import *
+from .storage import global_must_get, global_get_else, local_get_else, local_must_get
