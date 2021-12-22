b6b08c4d39ba71b13aedb653bf5efd0eea9e7ac2
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-22 13:06:46 -0500

    idk what im doing

---
+++ pytealutils/iter/iter_test.py
@@ -0,0 +1,31 @@
+from pyteal import *
+from iter import iterate
+
+
+def test_iterate():
+    res = iterate(Log(Bytes("a")), Int(10))
+    assert type(res) is SubroutineCall
+    assert res is not None
+
+    src = compile_app(res)
+    assert len(src) > 0
+
+
+def test_iterate_with_closure():
+
+    i = ScratchVar()
+
+    @Subroutine(TealType.none)
+    def logthing():
+        return Log(Itob(i.load()))
+
+    res = iterate(logthing(), Int(10), i)
+    assert type(res) is SubroutineCall
+    assert res is not None
+
+    src = compile_app(res)
+    assert len(src) > 0
+
+
+def compile_app(method: Expr):
+    return compileTeal(Seq(method, Int(1)), mode=Mode.Application, version=5)
