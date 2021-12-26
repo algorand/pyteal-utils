e8b9ae79cc49842fce79040697a8e8dc1622af44
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-26 07:38:00 -0500

    adding tests for math and strings

---
+++ pytealutils/nums/test_math.py
@@ -0,0 +1,29 @@
+from pyteal import Int, Itob, Log
+
+from tests.conftest import assert_output, logged_int
+
+from .math import div_ceil, exp10, max, min
+
+
+def test_exp10():
+    expr = Log(Itob(exp10(Int(3))))
+    output = [logged_int(int(1e3))]
+    assert_output(expr, output)
+
+
+def test_min():
+    expr = Log(Itob(min(Int(100), Int(10))))
+    output = [logged_int(int(10))]
+    assert_output(expr, output)
+
+
+def test_max():
+    expr = Log(Itob(max(Int(100), Int(10))))
+    output = [logged_int(int(100))]
+    assert_output(expr, output)
+
+
+def test_div_ceil():
+    expr = Log(Itob(div_ceil(Int(100), Int(3))))
+    output = [logged_int(int(34))]
+    assert_output(expr, output)
