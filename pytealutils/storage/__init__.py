2e91cb62b689cee0fe29ae8c0becfd3916729e50
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-26 08:01:53 -0500

    Fmt again?

--- pytealutils/storage/__init__.py
+++ pytealutils/storage/__init__.py
@@ -1,2 +1,3 @@
 from .blob import Blob, max_bytes, max_keys, page_size
-from .storage import global_get_else, global_must_get, local_get_else, local_must_get
+from .storage import (global_get_else, global_must_get, local_get_else,
+                      local_must_get)
