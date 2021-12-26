65d12cd3727f9f8e06be60b6d41ae41055b943a0
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-26 12:03:43 -0500

    fmt

---
+++ pytealutils/storage/test_blob.py
@@ -0,0 +1,23 @@
+from .blob import Blob
+
+b = Blob()
+
+
+def test_zero():
+    pass
+
+
+def test_write_bytes():
+    pass
+
+
+def test_read_bytes():
+    pass
+
+
+def test_write_byte():
+    pass
+
+
+def test_read_byte():
+    pass
