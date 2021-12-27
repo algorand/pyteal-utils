e6f56fc251ebc6218ad1d2f30ab87a3923471d85
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-27 09:02:14 -0500

    adding inline asm and fmt

---
+++ pytealutils/inline/test_inline_asm.py
@@ -0,0 +1,13 @@
+from algosdk.future.transaction import StateSchema
+from pyteal import *
+
+from tests.conftest import *
+
+
+
+def test_custom_op():
+
+    expr = Seq(Log(Bytes("hi")))
+
+    expected = [logged_bytes("hi")]
+    assert_output(expr, expected, global_schema=StateSchema(0, 64))
---
+++ pytealutils/storage/test_local_blob.py
@@ -0,0 +1,34 @@
+from pyteal import Bytes, Int, Itob, Log, Pop, Seq
+
+from tests.conftest import assert_stateful_output, logged_bytes, logged_int
+
+from .local_blob import LocalBlob
+
+# Can re-use the same blob
+b = LocalBlob()
+
+
+def test_local_blob_zero():
+    expr = Seq(b.zero(Int(0)), Log(b.read(Int(0), Int(0), Int(64))))
+    expected = [logged_int(0) * 8]
+    assert_stateful_output(expr, expected)
+
+
+def test_local_blob_write_read_bytes():
+    expr = Seq(
+        b.zero(Int(0)),
+        Pop(b.write(Int(0), Int(0), Bytes("deadbeef" * 8))),
+        Log(b.read(Int(0), Int(32), Int(40))),
+    )
+    expected = [logged_bytes("deadbeef")]
+    assert_stateful_output(expr, expected)
+
+
+def test_local_blob_write_read_byte():
+    expr = Seq(
+        b.zero(Int(0)),
+        b.set_byte(Int(0), Int(32), Int(123)),
+        Log(Itob(b.get_byte(Int(0), Int(32)))),
+    )
+    expected = [logged_int(123)]
+    assert_stateful_output(expr, expected)
