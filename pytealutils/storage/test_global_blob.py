01b03327df375bc95ad74d46135bfdb4a7174d8d
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-27 09:02:14 -0500

    adding inline asm and fmt

---
+++ pytealutils/storage/test_global_blob.py
@@ -0,0 +1,31 @@
+from algosdk.future.transaction import StateSchema
+from pyteal import Int, Itob, Log, Seq
+
+from tests.conftest import assert_output, logged_int
+
+from .global_blob import GlobalBlob
+
+# Can re-use the same blob
+b = GlobalBlob()
+
+
+def test_global_blob_zero():
+    expr = Seq(b.zero(), Log(b.read(Int(0), Int(64))))
+    expected = [logged_int(0) * 8]
+    assert_output(expr, expected, global_schema=StateSchema(0, 64))
+
+
+# def test_global_blob_write_read_bytes():
+#    expr = Seq(
+#        b.zero(),
+#        Pop(b.write(Int(0), Bytes("deadbeef" *2))),
+#        Log(b.read(Int(0), Int(8))),
+#    )
+#    expected = [logged_bytes("deadbeef")]
+#    assert_output(expr, expected, global_schema=StateSchema(0, 64))
+
+
+def test_global_blob_write_read_byte():
+    expr = Seq(b.zero(), b.set_byte(Int(32), Int(123)), Log(Itob(b.get_byte(Int(32)))))
+    expected = [logged_int(123)]
+    assert_output(expr, expected, global_schema=StateSchema(0, 64))
