dbfc743f04902ca5bf91bd1c85ff121a275aac94
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-01-09 19:30:30 -0500

    nothing

--- pytealutils/transaction/match.py
+++ pytealutils/transaction/match.py
@@ -13,11 +13,43 @@ from pyteal import (
     TxnField,
     TxnType,
 )
+# Common field checks
+
+NoRekey = {TxnField.rekey_to: Global.zero_address()}
+NoCloseTo = {TxnField.close_remainder_to: Global.zero_address()}
+NoCloseAsset = {TxnField.asset_close_to: Global.zero_address()}
+Common = {**NoRekey, **NoCloseTo, **NoCloseAsset}
+
+Payment = {TxnField.type_enum: TxnType.Payment}
+AssetTransfer = {TxnField.type_enum: TxnType.AssetTransfer}
+AssetConfig = {TxnField.type_enum: TxnType.AssetConfig}
+
+ToMe = {TxnField.receiver: Global.current_application_address()}
+FromMe = {TxnField.sender: Global.current_application_address()}
+
+def PaymentAmount(amount: Union[int, Int]) -> Dict[TxnField, Expr]:
+    if type(amount) == int:
+        amount = Int(amount)
+    return {**Payment, TxnField.amount: amount}
+
+
+def AssetIdAndAmount(
+    id: Union[int, Int], amount: Union[int, Int]
+) -> Dict[TxnField, Expr]:
+    if type(id) == int:
+        id = Int(id)
+
+    if type(amount) == int:
+        amount = Int(amount)
+
+    return {**AssetTransfer, TxnField.xfer_asset: id, TxnField.asset_amount: amount}
 
 
 class Match(Expr):
-    def __init__(self, *txns: Dict[TxnField, Expr]):
+    """Match checks a transaction group against a set of transaction field to expression mapping.
 
+    """
+    def __init__(self, *txns: Dict[TxnField, Expr]):
         filters = []
         for idx, txn in enumerate(txns):
             filters.append(And(*[GtxnExpr(idx, k) == v for k, v in txn.items()]))
@@ -36,33 +68,3 @@ class Match(Expr):
     def __str__(self):
         return "Matcher TODO..."
 
-
-NoRekey = {TxnField.rekey_to: Global.zero_address()}
-NoCloseTo = {TxnField.close_remainder_to: Global.zero_address()}
-NoCloseAsset = {TxnField.asset_close_to: Global.zero_address()}
-Common = {**NoRekey, **NoCloseTo, **NoCloseAsset}
-
-Payment = {TxnField.type_enum: TxnType.Payment}
-AssetTransfer = {TxnField.type_enum: TxnType.AssetTransfer}
-AssetConfig = {TxnField.type_enum: TxnType.AssetConfig}
-
-ToMe = {TxnField.receiver: Global.current_application_address()}
-FromMe = {TxnField.sender: Global.current_application_address()}
-
-
-def PaymentAmount(amount: Union[int, Int]) -> Dict[TxnField, Expr]:
-    if type(amount) == int:
-        amount = Int(amount)
-    return {**Payment, TxnField.amount: amount}
-
-
-def AssetIdAndAmount(
-    id: Union[int, Int], amount: Union[int, Int]
-) -> Dict[TxnField, Expr]:
-    if type(id) == int:
-        id = Int(id)
-
-    if type(amount) == int:
-        amount = Int(amount)
-
-    return {**AssetTransfer, TxnField.xfer_asset: id, TxnField.asset_amount: amount}
--- pytealutils/transaction/test_match.py
+++ pytealutils/transaction/test_match.py
@@ -11,6 +11,10 @@ def test_match():
     payMe = {**Common, **ToMe, **PaymentAmount(100)}
     giveThing = {**Common, **FromMe, **AssetIdAndAmount(10, 100)}
 
+
+    #TODO: I want to add constraints across transactions
+    # ex giveThing.sender == payMe.receiver && payMe.sender == giveThing.receiver
+
     # In cond, check if the txns match and route to the right method, in this case just approve
     expr = Cond([Match(payMe, giveThing), Approve()])
 
