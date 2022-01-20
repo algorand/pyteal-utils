21f1a4cba381e1e39ee0a82c93cdb0c1964e669b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-01-20 06:40:02 -0500

    guh

--- pytealutils/inline/test_inline_asm.py
+++ pytealutils/inline/test_inline_asm.py
@@ -23,5 +23,5 @@ extract a b
     s = ScratchSlot()
     expr = Seq(InlineAssembly(get_uint8, Itob(Int(255))), s.store(), Log(s.load()))
 
-    expected = ["unable to parse"]
+    expected = [INVALID_SYNTAX]  # TODO: in the next version, this will be "unable to parse"
     assert_fail(expr, expected)
