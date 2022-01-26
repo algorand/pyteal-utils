0674b610afaa9fefe8e740fd29a9970393f2645f
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-01-26 13:21:36 -0500

    adding debug logger

--- pytealutils/debug/debug.py
+++ pytealutils/debug/debug.py
@@ -1,4 +1,4 @@
-from pyteal import Bytes, Concat, Global, Log
+from pyteal import Bytes, Concat, Global, InnerTxn, Log, If, Int
 
 from ..strings import itoa
 
@@ -6,13 +6,12 @@ from ..strings import itoa
 def log_stats():
     return Log(
         Concat(
-            Bytes("Current App Id: "),
+            Bytes("\nCurrent App Id: "),
             itoa(Global.current_application_id()),
-            Bytes("Caller App Id: "),
-            itoa(Global.caller_app_id()),
-            Bytes("Budget: "),
+            Bytes("\nBudget: "),
             itoa(Global.opcode_budget()),
-            Bytes("Group size: "),
+            Bytes("\nGroup size: "),
             itoa(Global.group_size()),
+            If(Global.caller_app_id()>Int(0), Concat(Bytes("\nCaller App Id: "), itoa(Global.caller_app_id())), Bytes("")),
         )
     )
