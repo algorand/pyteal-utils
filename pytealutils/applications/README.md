e14f7cfd3a956ed7c6c3a100df3e41d3d9e8e0f5
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-06 07:23:32 -0400

    some comments, needs much better docs

---
+++ pytealutils/applications/README.md
@@ -0,0 +1,6 @@
+Application Structure
+---------------------
+
+*Warning*: This is an experimental set of features.
+
+Please use with caution and provide feedback as issues
--- pytealutils/applications/application.py
+++ pytealutils/applications/application.py
@@ -47,6 +47,8 @@ def add_handler_config(fn: HandlerFunc | ABIReturnSubroutine, key: str, val: Any
 
 
 class PrependExpr:
+    """Prepends an expression to be evaluated prior to the body of the method"""
+
     def __init__(self, fn, expr):
         self.fn = fn
         self.expr = expr
@@ -63,26 +65,13 @@ class PrependExpr:
         return _impl
 
 
-class AppendExpr:
-    def __init__(self, fn, expr):
-        self.fn = fn
-        self.expr = expr
-
-    def __get__(self):
-        def _impl(*args, **kwargs):
-            return Seq(self.fn(*args, **kwargs), self.expr)
-
-        _impl.__name__ = self.fn.__name__
-        _impl.__annotations__ = self.fn.__annotations__
-        _impl.__signature__ = signature(self.fn)
-        _impl.__doc__ = self.fn.__doc__
-
-        return _impl
-
-
 class Authorize:
+    """Authorize contains methods that may be used as values to the `authorize` keyword of the `handle` decorator"""
+
     @staticmethod
     def only(addr: Expr):
+        """only requires that the sender of the app call being evaluated match exactly the address passed"""
+
         @Subroutine(TealType.uint64, name="auth_only")
         def _impl(sender: Expr):
             return sender == addr
@@ -91,6 +80,8 @@ class Authorize:
 
     @staticmethod
     def holds_token(asset_id: Expr):
+        """holds_token ensures that the sender of the app call being evaluated holds >0 of the asset id passed"""
+
         @Subroutine(TealType.uint64, name="auth_holds_token")
         def _impl(sender: Expr):
             return Seq(
@@ -102,6 +93,8 @@ class Authorize:
 
     @staticmethod
     def opted_in(app_id: Expr):
+        """opted_in ensures that the sender of the app call being evaluated has already opted-in to a given app id"""
+
         @Subroutine(TealType.uint64, name="auth_opted_in")
         def _impl(sender: Expr):
             return App.optedIn(sender, app_id)
@@ -145,6 +138,8 @@ def _on_complete(mc: MethodConfig):
 
 
 def internal(return_type: TealType):
+    """internal can be used to wrap a subroutine that is defined inside an application class"""
+
     def _impl(fn: HandlerFunc):
         return Subroutine(return_type)(fn)
 
@@ -155,11 +150,21 @@ def handler(
     fn: HandlerFunc = None,
     /,
     *,
-    authorize: Expr = None,
+    authorize: SubroutineFnWrapper = None,
     method_config: MethodConfig = None,
     read_only: bool = False,
-    assert_zero: list[TxnField] = None,
 ):
+    """
+    handler is the primary way to expose an ABI method for an application
+    it may take a number of arguments:
+
+    Args:
+
+        authorize: A subroutine that should evaluate to 1/0 depending on the app call transaction sender
+        method_config: accepts a MethodConfig object to define how the app call should be routed given OnComplete and whether or not the call is a create
+        read_only: adds read_only flag to abi (eventually, currently it does nothing)
+    """
+
     def _impl(fn: HandlerFunc):
         if authorize is not None:
             fn = _authorize(authorize)(fn)
@@ -167,8 +172,6 @@ def handler(
             fn = _on_complete(method_config)(fn)
         if read_only:
             fn = _readonly(fn)
-        if assert_zero is not None and len(assert_zero) > 0:
-            fn = _assert_zero(assert_zero)(fn)
 
         wrapped = ABIReturnSubroutine(fn)
         setattr(wrapped, _handler_config_attr, get_handler_config(fn))
@@ -181,14 +184,16 @@ def handler(
 
 
 class EmptyAppState(ApplicationState):
-    pass
+    """Default app state for Application class, no class variables defined"""
 
 
 class EmptyAccountState(AccountState):
-    pass
+    """Default account state for Application class, no class variables defined"""
 
 
 class Application:
+    """Application should be subclassed to add functionality"""
+
     app_state: ApplicationState = EmptyAppState()
     acct_state: AccountState = EmptyAccountState()
 
@@ -200,7 +205,7 @@ class Application:
         attrs = [
             getattr_static(self, m)
             for m in list(set(dir(self.__class__)) - set(dir(super())))
-            if not m.startswith("_")
+            if not m.startswith("__")
         ]
 
         self.router = Router(type(self).__name__)
